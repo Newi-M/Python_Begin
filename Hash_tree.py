fd3c1a2565994ea88b8b7db0c0e8204d7de4a6cb
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2024-02-22 21:49:49 +0400

    Hash symbol tree

--- Hash_tree.py
+++ Hash_tree.py
@@ -4,7 +4,7 @@ row = int(input('How  tall do you want the Hash-Tree be? (Enter number of rows) 
 # Create a variable of value 1 which represent by how much the number of Hash increases in each iteration
 hash_increase = 1
 
-# Iterate printing space and hash sign in each row
+# Iterate printing space and hash symbol in each row
 for i in range(row):
 
     space_no = row - (i+1)
