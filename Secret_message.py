ea9789735eb0625ffefa5d59a68cafeedb5cfca5
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2024-02-23 12:53:35 +0400

    Convert to ASCII and return back

---
+++ Secret_message.py
@@ -0,0 +1,19 @@
+message = input('Enter a word in upper case: ')
+
+# String to hold the ascii value of each letter of the word
+secret_code = ""
+for char in message:
+    secret_code += str(ord(char))
+
+print("The secret message is: ", secret_code)
+
+# String to hold the two digits of the secret message
+two_digit = ""
+
+# String to hold the original message
+original_message = ""
+for num in range(0, len(secret_code) - 1, 2):
+    two_digit = secret_code[num] + secret_code[num + 1]
+    original_message += chr(int(two_digit))
+
+print("Original message is: ", original_message)
